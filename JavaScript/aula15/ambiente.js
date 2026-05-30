1f3a3259dbbcddf2ff2842d2b230da1375619a73
rafael <157124158+zoneplay18@users.noreply.github.com>
2026-05-30 11:14:28 -0300

    .

--- JavaScript/aula15/ambiente.js
+++ JavaScript/aula15/ambiente.js
@@ -1,3 +1,12 @@
 let num = [5, 7, 8, 9, 1]
-
-console.log(`Nosso vetor é o ${num}`)
+num.push(2)
+num.sort()
+console.log(num)
+console.log(`O meu vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetro é ${num[0]}`)
+let pos = num.indexOf(1)
+if (pos == -1) {
+    console.log('O veotr não foi encontrado')
+} else {
+    console.log(`Ele está na posição ${pos}`)
+}
